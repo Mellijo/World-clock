f6bd51f2fac94e8dcec7676643d59bcd2ac787e7
Mellijo <118224647+Mellijo@users.noreply.github.com>
2023-02-03 22:34:53 +0200

    js added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
 
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let edinburghElement = document.querySelector("#edinburgh");
+  let edinburghDateElement = edinburghElement.querySelector(".date");
+  let edinburghTimeElement = edinburghElement.querySelector(".time");
+
+  let edinburghTime = moment().tz("GB");
+
+  edinburghDateElement.innerHTML = edinburghTime.format("MMMM Do YYYY");
+
+  edinburghTimeElement.innerHTML = edinburghTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let harareElement = document.querySelector("#harare");
+  let harareDateElement = harareElement.querySelector(".date");
+  let harareTimeElement = harareElement.querySelector(".time");
+
+  let harareTime = moment().tz("Africa/Harare");
+
+  harareDateElement.innerHTML = harareTime.format("MMMM Do YYYY");
+
+  harareTimeElement.innerHTML = harareTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
